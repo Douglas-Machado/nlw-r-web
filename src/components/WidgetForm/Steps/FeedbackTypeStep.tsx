b34ed5e228b095c05e60a753427f617c4a35f547
Douglas C Machado <douglasclaudino10.dc@gmail.com>
2022-05-05 16:02:01 -0300

    feddback form components

--- src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -1,8 +1,8 @@
-import { feedbackType, feedbackTypes } from ".."
+import { FeedbackType, feedbackTypes } from ".."
 import { CloseButton } from "../../CloseButton";
 
 interface FeedbackTypeStepProps {
-  onFeedbackTypeChanged: (type: feedbackType) => void;
+  onFeedbackTypeChanged: (type: FeedbackType) => void;
 }
 
 export function FeedbackTypeStep({ onFeedbackTypeChanged }: FeedbackTypeStepProps) {
@@ -18,8 +18,11 @@ export function FeedbackTypeStep({ onFeedbackTypeChanged }: FeedbackTypeStepProp
         return(
           <button 
             key={key}
-            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-            onClick={() => onFeedbackTypeChanged(key as feedbackType)}
+            className="
+              bg-zinc-800 rounded-lg 
+                py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent
+              hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+            onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
           >
             <img src={value.image.source} alt={value.image.alt} />
             <span>{value.title}</span>
--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,5 +1,4 @@
 import { Heart } from "phosphor-react"
-import { CloseButton } from '../CloseButton'
 
 import bugImageUrl from '../../assets/bug.svg'
 import ideaImageUrl from '../../assets/idea.svg'
@@ -7,6 +6,7 @@ import thoughtImageUrl from '../../assets/thought.svg'
 import { useState } from "react"
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep"
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep"
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep"
 
 
 export const feedbackTypes = {
@@ -33,25 +33,49 @@ export const feedbackTypes = {
   }
 }
 
-export type feedbackType = keyof typeof feedbackTypes
+export type FeedbackType = keyof typeof feedbackTypes
 
 export function WidgetForm() {
-  const [feedbackType, setFeedbackType] = useState<feedbackType | null>(null)
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+  const [feedbackSent, setFeedbackSent] = useState(false)
+
+  function handleRestartFeedback() {
+    setFeedbackSent(false)
+    setFeedbackType(null)
+  }
 
   return (
-    <div className="bg-zinc-900 pt-4 pl-4 pr-4 pb-3 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-
-      {!feedbackType ? (
-        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
+    <div 
+      className="
+      bg-zinc-900 pt-4 pl-4 pr-4 pb-3 relative 
+        rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto
+      ">
+      { feedbackSent  ? (
+        <FeedbackSuccessStep
+          onFeedbackRestartRequested={handleRestartFeedback} 
+        />
       ) : (
-        <FeedbackContentStep feedbackType={feedbackType} />
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
+          ) : (
+            <FeedbackContentStep 
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
       )}
 
 
       <footer className="text-[#f7b18b] flex">
         Feito com 
         <Heart weight="fill" />  
-        <a className="underline underline-offset-2" href="https://github.com/Douglas-Machado">Douglas C. Machado</a>
+        <a 
+          className="underline underline-offset-2" 
+          href="https://github.com/Douglas-Machado
+        ">Douglas C. Machado</a>
       </footer>
     </div>
   )
